516c322570027f44700cee74867781b92c18e74a
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2022-04-06 19:59:09 -0300

    Criando tela de Splash

--- UI/Program.cs
+++ UI/Program.cs
@@ -19,7 +19,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmSplash());
         }
     }
 }
